5dad667cb0cb61f765fa6cf473fa8f9cbb446d82
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-09-06 22:00:30 -0700

    created constructors and destructor

--- Battle.h
+++ Battle.h
@@ -1,6 +1,29 @@
 #ifndef BATTLE_H
 #define BATTLE_H
 
-class Battle {};
+#include "Player.h"
+
+class Battle {
+    private:
+        Player one_;
+        Player two_;
+    public:
+        Battle() : one_(), two_() {}
+
+        Battle(const Player& one, const Player& two)
+        : one_(one), two_(two) {}
+
+        Battle(const Battle& other)
+        : one_(other.one_), two_(other.two_) {}
+
+        ~Battle() {}
+
+        Battle& operator=(const Battle& other) {
+            one_ = other.one_;
+            two_ = other.two_;
+
+            return  *this;
+        }
+};
 
 #endif
