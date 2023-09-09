4ed78da446e7cb7be44d961bb278328ccb820a7f
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-09-08 21:40:38 -0700

    added current health variable

--- Character.h
+++ Character.h
@@ -7,24 +7,26 @@ class Character {
     private:
         std::string name_;
         int health_;
+        int currentHealth_;
         int attack_;
         int defense_;
         int speed_;
     public:
         Character() 
-        : name_("NULL"), health_(100), attack_(0), defense_(0), speed_(0) {}
+        : name_("NULL"), health_(100), currentHealth_(100), attack_(0), defense_(0), speed_(0) {}
 
-        Character(const std::string& nm, const int& hp, const int& atk, const int& df, const int& sp)
-        : name_(nm), health_(hp), attack_(atk), defense_(df), speed_(sp) {}
+        Character(const std::string& nm, const int& hp, const int& chp, const int& atk, const int& df, const int& sp)
+        : name_(nm), health_(hp), currentHealth_(chp), attack_(atk), defense_(df), speed_(sp) {}
 
         Character(const Character& other)
-        : name_(other.name_), health_(other.health_), attack_(other.attack_), defense_(other.defense_), speed_(other.speed_) {}
+        : name_(other.name_), health_(other.health_), currentHealth_(other.currentHealth_), attack_(other.attack_), defense_(other.defense_), speed_(other.speed_) {}
 
         ~Character() {}
 
         Character& operator=(const Character& other) {
             name_= other.name_;
             health_ = other.health_;
+            currentHealth_ = other.currentHealth_;
             attack_ = other.attack_;
             defense_ = other.defense_;
             speed_ = other.speed_;
@@ -38,6 +40,10 @@ class Character {
 
         int getHealth() const { 
             return health_; 
+        }
+
+        int getCurrentHealth() const {
+            return currentHealth_;
         }
     
         int getAttack() const { 
@@ -58,6 +64,10 @@ class Character {
 
         void setHealth(const int& health) {
             health_ = health;
+        }
+
+        void setCurrentHealth(const int& currentHealth) {
+            currentHealth_ = currentHealth;
         }
 
         void setAttack(const int& attack) { 
