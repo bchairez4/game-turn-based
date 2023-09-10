0bd409fe63e3841b82633a06d8c61127805c321d
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-09-09 20:28:48 -0700

    defined general skeleton

--- Game.h
+++ Game.h
@@ -1,6 +1,20 @@
 #ifndef GAME_H
 #define GAME_H
 
-class Game {};
+#include <vector>
+#include "Battle.h"
+
+class Game {
+    private:
+        //
+    public:
+        Game() {}
+        Game(const Game& other) {}
+        ~Game() {}
+        Game& operator=(const Game& other) {}
+        void characterSelection();
+        void start();
+        void results();
+};
 
 #endif
