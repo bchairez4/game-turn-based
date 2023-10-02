df6051a056a8b483c7f5b1cef9a25b87387b6303
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-10-01 23:05:36 -0700

    created NPC.h

---
+++ NPC.h
@@ -0,0 +1,30 @@
+#ifndef NPC_H
+#define NPC_H
+
+#include "Player.h"
+
+class NPC : public Player {
+    private:
+        //
+    public:
+        NPC() {}
+
+        NPC(const Character& character, const std::vector<Item>& itemList) {
+            setCurrent(character);
+            setItems(itemList);
+        }
+
+        NPC(const NPC& other) {
+            setCurrent(other.getCurrent());
+            setItems(other.getItems());
+        }
+
+        ~NPC() {}
+
+        NPC& operator=(const NPC& other) {
+            setCurrent(other.getCurrent());
+            setItems(other.getItems());
+        }
+};
+
+#endif
